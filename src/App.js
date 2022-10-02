a4ee2b9cf7dce382207a18988dc4c620e6ca3de9
hasansikdar <hasansikdar973@gmail.com>
2022-10-02 10:57:37 +0600

    class 15 react icons uses

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
+import { FaFacebookF, FaYoutube } from "react-icons/fa";
+import { HiCamera,HiArrowSmRight } from "react-icons/hi";
+import { IoLogoFacebook } from "react-icons/io5";
 import './App.css';
-import { useEffect, useState } from 'react';
-import Card1 from './Components/Card/Card1';
-import Card2 from './Components/Card/Card2';
-import ClassComponent from './Components/Card/ClassComponent';
-import Card3 from './Components/Card/Card3';
-import Card4 from './Components/Card/Card4';
-import Card5 from './Components/Card/Card5';
-import Card6 from './Components/Card/Card6';
+import Card1 from './component/Cards/Card1';
 function App() {
   
   return (
     <div className="App">
         <Card1></Card1>
-        <Card2></Card2>
-        <ClassComponent></ClassComponent>
-        <Card3></Card3>
-        <Card4></Card4>
-        <Card5></Card5>
-        <Card6></Card6>
+        <span><FaFacebookF /></span>
+        <span><FaYoutube /></span>
+        <span><HiCamera /></span>
+        <button>Read More <HiArrowSmRight /></button>
+        <span><IoLogoFacebook/></span>
     </div>
   );
 }
