0ba6401bfbfd14656bde92d36b21273447a65908
hasansikdar <hasansikdar973@gmail.com>
2022-10-02 09:55:38 +0600

    class 11

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        users.map((user, index) => {
+          const {name, email, phone, website} = user;
+          console.log(user);
+          return <div key={index} className="user">
+            <h3>{name}</h3>
+            <p>Email: {email}</p>
+            <p>Phone: {phone}</p>
+            <p>Website: <a target="_blank" href={`http://${website}`}>{website}</a></p>
+          </div> 
+        })
+      }
     </div>
   );
 }
